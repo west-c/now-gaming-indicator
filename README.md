2d992bf684e9dc174e02b9f06143b44982014527
west-c <3ys.drop@gmail.com>
2023-11-10 14:06:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## now-gaming-indicator
+
+今遊んでいるゲーム作品（Notionで管理）を、Misskeyのプロフィールに定期的に反映するスクリプト
---
+++ index.ts
@@ -0,0 +1,65 @@
+import { Client } from '@notionhq/client'
+import axios from 'axios';
+
+const NOTION_SECRET = process.env.NOTION_SECRET
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID!
+const MISSKEY_TOKEN = process.env.MISSKEY_TOKEN
+const MISSKEY_API_URL = process.env.MISSKEY_API_URL
+
+const notion = new Client({
+  auth: NOTION_SECRET,
+})
+
+async function getGames() {
+  const myDatabase = await notion.databases.query({
+    database_id: NOTION_DATABASE_ID,
+    filter: {
+      property: 'Status',
+      select: {
+        equals: 'やってる',
+      },
+    },
+  })
+  const results: any[] = myDatabase.results
+  const list: string[] = results.map(page => page.properties.Title.title[0].plain_text)
+  return list
+}
+
+async function getProfile() {
+  const postData = {
+    i: MISSKEY_TOKEN,
+  };
+  
+  const response = await axios.post(MISSKEY_API_URL + '/i', postData)
+  return response.data
+}
+
+async function updateProfile(games: string[], profile: any) {
+  let fields: any[] = profile.fields.filter((field: any) => field.name !== 'Now Gaming')
+  fields.push({
+      name: 'Now Gaming',
+      value: games.join(' / '),
+    })
+
+  const postData = {
+    i: MISSKEY_TOKEN,
+    fields: fields,
+  };
+  
+  axios.post(MISSKEY_API_URL + '/i/update', postData)
+    .then(() => {
+      console.log('Updated.');
+    })
+    .catch(error => {
+      console.error(error)
+    });
+}
+
+async function main() {
+  console.log(NOTION_SECRET)
+  const games: string[] = await getGames()
+  const profile: any = await getProfile()
+  await updateProfile(games, profile)
+}
+
+main()
