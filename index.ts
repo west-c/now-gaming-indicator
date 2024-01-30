cc4e8aafd21682cd79cd8bfb8e5e856f35108938
west-c <3ys.drop@gmail.com>
2024-01-30 10:25:33 +0900

    ESLintの導入

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 import { Client, isFullPage } from '@notionhq/client'
-import { QueryDatabaseResponse, RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
+import { type QueryDatabaseResponse, type RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
 import axios from 'axios'
 
-const NOTION_SECRET = process.env.NOTION_SECRET
-const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID!
-const MISSKEY_TOKEN = process.env.MISSKEY_TOKEN
-const MISSKEY_API_URL = process.env.MISSKEY_API_URL
+const NOTION_SECRET: string = process.env.NOTION_SECRET ?? ''
+const NOTION_DATABASE_ID: string = process.env.NOTION_DATABASE_ID ?? ''
+const MISSKEY_TOKEN: string = process.env.MISSKEY_TOKEN ?? ''
+const MISSKEY_API_URL: string = process.env.MISSKEY_API_URL ?? ''
 
 const notion = new Client({
-  auth: NOTION_SECRET,
+  auth: NOTION_SECRET
 })
 
 type TitleObject = {
-  type: "title"
-  title: Array<RichTextItemResponse>
+  type: 'title'
+  title: RichTextItemResponse[]
   id: string
 }
 
@@ -26,15 +26,15 @@ type Field = {
   value: string
 }
 
-async function getGames(): Promise<string[]> {
+async function getGames (): Promise<string[]> {
   const myDatabase: QueryDatabaseResponse = await notion.databases.query({
     database_id: NOTION_DATABASE_ID,
     filter: {
       property: 'Status',
       select: {
-        equals: 'やってる',
-      },
-    },
+        equals: 'やってる'
+      }
+    }
   })
 
   return myDatabase.results.map(page => {
@@ -46,36 +46,34 @@ async function getGames(): Promise<string[]> {
   })
 }
 
-async function getProfile(): Promise<Profile> {
+async function getProfile (): Promise<Profile> {
   const response = await axios.post(MISSKEY_API_URL + '/i', {
-    i: MISSKEY_TOKEN,
+    i: MISSKEY_TOKEN
   })
   return response.data as Profile
 }
 
-async function updateProfile(games: string[], profile: Profile): Promise<void> {
+async function updateProfile (games: string[], profile: Profile): Promise<void> {
   const fields: Field[] = profile.fields.filter(field => field.name !== 'Now Gaming')
   fields.push({
-      name: 'Now Gaming',
-      value: games.join(' / '),
-    })
+    name: 'Now Gaming',
+    value: games.join(' / ')
+  })
 
   axios.post(MISSKEY_API_URL + '/i/update', {
-      i: MISSKEY_TOKEN,
-      fields: fields,
-    })
+    i: MISSKEY_TOKEN,
+    fields
+  })
     .then(() => {
-      console.log('Updated.');
+      console.log('Updated.')
     })
     .catch(error => {
       console.error(error)
     })
 }
 
-async function main(): Promise<void> {
+(async function main (): Promise<void> {
   const games: string[] = await getGames()
   const profile: Profile = await getProfile()
   await updateProfile(games, profile)
-}
-
-main()
+})()
